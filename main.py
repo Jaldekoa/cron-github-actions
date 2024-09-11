4c22f7ff7b0a3a04e8f955abfa168487719bce7c
Jon Aldekoa <jaldekoa@gmail.com>
2024-09-11 15:57:00 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,12 @@
+from plotnine import ggplot, geom_point, aes, stat_smooth, facet_wrap, ggtitle
+from plotnine.data import mtcars
+import datetime
+
+plot = (ggplot(mtcars, aes("wt", "mpg", color="factor(gear)"))
+        + geom_point()
+        + stat_smooth(method="lm")
+        + facet_wrap("gear")
+        + ggtitle(f"{datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")}")
+        )
+
+plot.save(filename="plotnine-save.jpg", format="jpg")
